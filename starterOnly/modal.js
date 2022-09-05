988f3e9f66168711cfa09345fe116bdfd9316606
Soeun Lee <thdms1005@gmail.com>
2022-09-05 10:28:25 +0200

    error msgs done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,12 +18,14 @@ const form = document.querySelector('form');
 const fName = document.getElementById("first");
 const lName = document.getElementById("last");
 const enteredEmail = document.getElementById("email");
-const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
 const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const locationRadio = document.querySelectorAll(
   "input[name=location]"
 )
+const agreeBox = document.getElementById("checkbox1");
+// email regular expressions
+const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,16 +41,18 @@ closeIcon.addEventListener("click", ()=>{
 })
 
 
-// 1. First name & Last name validation.
+/* 
+  1. First name & Last name validation.
+  */
+
 function firstNameValidation() {
   if (fName.value == "" || fName.value.length < 2) {
         fName.parentNode.setAttribute("data-error-visible", "true");
       }
   else {
-    fName.removeAttribute("data-error-visible");
+    fName.parentNode.setAttribute("data-error-visible", "false");
   }
   return true;
-
 }
 
 function lastNameValidation() {
@@ -56,12 +60,16 @@ function lastNameValidation() {
         lName.parentNode.setAttribute("data-error-visible", "true");
       }
   else {
-    lName.removeAttribute("data-error-visible");
+    lName.parentNode.setAttribute("data-error-visible", "false");
+    // lName.removeAttribute("data-error-visible");
   }
   return true;
 }
 
-// 2. Email validation.
+
+/* 
+  2. Email validation.
+  */
 function emailValidation(){
   if(!enteredEmail.value.match(mailRegex)) {
     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
@@ -70,36 +78,43 @@ function emailValidation(){
     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
   }
   else {
-    enteredEmail.removeAttribute("data-error-visible");
+    enteredEmail.parentNode.setAttribute("data-error-visible", "false");
   }
   return true;
 }
 
-// 3. B-day validation.
+/* 
+  3. B-day validation.
+  */
 function bdayValidation() {
   if(birthdate.value == ""){
     birthdate.parentNode.setAttribute("data-error-visible", "true");
   }
   else{
-    birthdate.removeAttribute("data-error-visible");
+    birthdate.parentNode.setAttribute("data-error-visible", "false");
   }
   return true;
 }
 
-// 4. Participation quantity validation.
+/* 
+  4. Participation quantity validation.
+  */
 function quantityValidation() {
   if(quantity.value == "") {
     quantity.parentNode.setAttribute("data-error-visible", "true");
   }
   else {
-    quantity.removeAttribute("data-error-visible");
+    quantity.parentNode.setAttribute("data-error-visible", "false");
   }
   return true;
 }
 
-// 5. Location radio button validation.
+/* 
+  5. Location radio button validation. 
+  */
 function radioValidation() {
 let isChecked = false;
+  
   locationRadio.forEach((radio) => {
     if(radio.checked) {
       isChecked = true;
@@ -108,14 +123,29 @@ let isChecked = false;
   if(!isChecked) {
     locationRadio[0].parentNode.setAttribute("data-error-visible", "true");
   }
+  else {
+    locationRadio[0].parentNode.setAttribute("data-error-visible", "false");
+  }
 }
 
+ /* 
+  6. Checkbox validation. 
+  */
+function checkboxValidation() {
+  if(agreeBox.checked == false) {
+    agreeBox.parentNode.setAttribute("data-error-visible", "true");
+    console.log('it works');
+  }
+  else{
+    agreeBox.parentNode.setAttribute("data-error-visible", "false");
+  }
 
-// 6. Checkbox validation.
+}
 
-
-// 7. Submit button event.
-  // all validation functions combined. 
+ /* 
+  7. Submit button event.
+  All validation functions combined. 
+  */
 function validateAll(){
   firstNameValidation();
   lastNameValidation();
@@ -123,13 +153,21 @@ function validateAll(){
   bdayValidation();
   quantityValidation();
   radioValidation();
-
+  checkboxValidation();
 }
 
-// When submit btn is clicked -> validate all inputs.
+ /* 
+  When submit btn is clicked -> validate all inputs. 
+  */
 submitBtn.addEventListener("click", (validateAll));
 
 
+
+
+
+
+
+// older versions
 // submitBtn.addEventListener("submit", (e) => {
 //     // preventing default when submitted.
 //     e.defaultPrevented();
@@ -185,18 +223,6 @@ submitBtn.addEventListener("click", (validateAll));
 //     quantity.parentNode.setAttribute("data-error-visible", "true");
 //   }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // submitBtn.addEventListener("click", ()=>{
 //   if(enteredEmail.value.match(mailRegex)) {
