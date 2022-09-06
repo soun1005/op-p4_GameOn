73ef5754704efa3607fb2fe462bdb60e8489c81b
Soeun Lee <thdms1005@gmail.com>
2022-09-06 18:41:08 +0200

    confirm modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,7 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeIcon = document.querySelector(".close");
-const submitBtn = document.querySelector(".btn-submit");
+const submitBtn = document.getElementById("submit-form");
 const input = document.getElementsByTagName("input");
 const form = document.querySelector('form');
 const fName = document.getElementById("first");
@@ -24,8 +24,13 @@ const locationRadio = document.querySelectorAll(
   "input[name=location]"
 )
 const agreeBox = document.getElementById("checkbox1");
+const confirmModal = document.querySelector(".confirm-bg");
+const confirmModalText = document.querySelector(".confirm-modal");
+const confirmModalBtn = document.querySelector(".confirm-modal-btn");
+const formControl = document.getElementById("body-form");
 // email regular expressions
 const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -35,86 +40,117 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form with X icon
-closeIcon.addEventListener("click", ()=>{
+// close modal function
+function closeModal(){
   modalbg.style.display = "none";
+  confirmModal.style.display = "none";
+}
+
+// close modal by clicking 'X' icon
+closeIcon.addEventListener("click", ()=>{
+  closeModal();
 })
 
+// close modal by ESC key
+document.addEventListener('keydown', (e)=> {
+  if (e.key === 'Escape') {
+  closeModal();
+  }
+});
 
-/* 
-  1. First name & Last name validation.
-  */
+// close confirmation modal
+confirmModalBtn.addEventListener("click", () => 
+{
+  closeModal();
+});
 
-function firstNameValidation() {
+// close confirmation modal by ESC key
+confirmModalBtn.addEventListener("click", () => 
+{
+  if (e.key === 'Escape') {
+    closeModal();
+    }
+});
+
+
+
+/***************************************** 
+Validation check.
+
+1. First name & Last name validation.
+******************************************/
+
+const firstNameValidation = () => {
   if (fName.value == "" || fName.value.length < 2) {
         fName.parentNode.setAttribute("data-error-visible", "true");
+        return false;
       }
   else {
     fName.parentNode.setAttribute("data-error-visible", "false");
   }
-  return true;
 }
 
-function lastNameValidation() {
+
+const lastNameValidation = () => {
   if (lName.value == "" || lName.value.length < 2) {
         lName.parentNode.setAttribute("data-error-visible", "true");
+        return false;
       }
   else {
     lName.parentNode.setAttribute("data-error-visible", "false");
     // lName.removeAttribute("data-error-visible");
   }
-  return true;
 }
 
 
-/* 
-  2. Email validation.
-  */
-function emailValidation(){
+/*********************
+2. Email validation.
+**********************/
+const emailValidation = () => {
   if(!enteredEmail.value.match(mailRegex)) {
     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
+    return false;
   }
   if(enteredEmail.value == ""){
     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
+    return false;
   }
   else {
     enteredEmail.parentNode.setAttribute("data-error-visible", "false");
   }
-  return true;
 }
 
-/* 
-  3. B-day validation.
-  */
-function bdayValidation() {
+/********************
+3. B-day validation.
+*********************/
+const bdayValidation = () => {
   if(birthdate.value == ""){
     birthdate.parentNode.setAttribute("data-error-visible", "true");
+    return false;
   }
   else{
     birthdate.parentNode.setAttribute("data-error-visible", "false");
   }
-  return true;
 }
 
-/* 
-  4. Participation quantity validation.
-  */
-function quantityValidation() {
+/***************************************  
+4. Participation quantity validation.
+****************************************/
+const quantityValidation = () => {
   if(quantity.value == "") {
     quantity.parentNode.setAttribute("data-error-visible", "true");
+    return false;
   }
   else {
     quantity.parentNode.setAttribute("data-error-visible", "false");
   }
-  return true;
 }
 
-/* 
-  5. Location radio button validation. 
-  */
-function radioValidation() {
+/*************************************
+5. Location radio button validation. 
+**************************************/
+const radioValidation = () => {
 let isChecked = false;
-  
   locationRadio.forEach((radio) => {
     if(radio.checked) {
       isChecked = true;
@@ -122,31 +158,37 @@ let isChecked = false;
   });
   if(!isChecked) {
     locationRadio[0].parentNode.setAttribute("data-error-visible", "true");
+    return false;
   }
   else {
     locationRadio[0].parentNode.setAttribute("data-error-visible", "false");
   }
 }
 
- /* 
-  6. Checkbox validation. 
-  */
-function checkboxValidation() {
+/***********************
+6. Checkbox validation. 
+***********************/
+const checkboxValidation = () => {
   if(agreeBox.checked == false) {
     agreeBox.parentNode.setAttribute("data-error-visible", "true");
-    console.log('it works');
+    return false;
   }
   else{
     agreeBox.parentNode.setAttribute("data-error-visible", "false");
   }
-
 }
 
- /* 
-  7. Submit button event.
-  All validation functions combined. 
-  */
-function validateAll(){
+/************************************
+7. Submit button.
+All validation functions combined.
+When submit btn is clicked -> validate all inputs. 
+************************************/
+
+const validate = (event) => {
+  // to prevent all inputs being default after 'c'est parti'
+  event.preventDefault();
+
+  //to trigger the annonymous functions for each inputs
   firstNameValidation();
   lastNameValidation();
   emailValidation();
@@ -154,86 +196,24 @@ function validateAll(){
   quantityValidation();
   radioValidation();
   checkboxValidation();
+
+  confirmModalOn();
+  } 
+
+/************************
+To open 'Confirm' modal.
+*************************/
+
+const confirmModalOn = () => {
+// when the validation failed
+const errorsVisible = document.querySelectorAll('[data-error-visible="true"]');
+console.log(errorsVisible);
+
+  const noError = errorsVisible.length === 0;
+  if(noError){
+    confirmModal.style.display= "block";
+    confirmModalText.style.display= "block";
+    modalbg.style.display = "none";
+  }
 }
-
- /* 
-  When submit btn is clicked -> validate all inputs. 
-  */
-submitBtn.addEventListener("click", (validateAll));
-
-
-
-
-
-
-
-// older versions
-// submitBtn.addEventListener("submit", (e) => {
-//     // preventing default when submitted.
-//     e.defaultPrevented();
-//     console.log('hello');    
-//     //  firstNameValidation() === true &&
-//     //   lastNameValidation() === true &&
-//     //   emailValidation() === true &&
-//     //   bdayValidation() === true &&
-//     //   quantityValidation() === true 
-//     //   successModal();
-   
-//   }) 
-
-
-
-// function successModal(){
-//   console.log(success);
-// }
-
-// function validate(event){
-//   // const form = document.querySelector('form');
-//   const fName = document.getElementById("first");
-//   const lName = document.getElementById("last");
-//   const enteredEmail = document.getElementById("email");
-//   const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
-//   const birthdate = document.getElementById("birthdate");
-//   const quantity = document.getElementById("quantity");
-
-//   event.preventDefault();
-//   // form.reset();
-
-//   // first and last name validation
-//   if (fName.value == "" || fName.value.length < 2) {
-//     fName.parentNode.setAttribute("data-error-visible", "true");
-//   }
-  
-//   if (lName.value == "" || lName.value.length < 2) {
-//     lName.parentNode.setAttribute("data-error-visible", "true");
-//   }
-
-//   // email validation
-//   if(!enteredEmail.value.match(mailRegex) || enteredEmail.value == "") {
-//     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
-//   }
-
-//   // email validation
-//   if(birthdate.value == ""){
-//     birthdate.parentNode.setAttribute("data-error-visible", "true");
-//   }
-
-//   // quantity validation
-//   if(quantity.value == "") {
-//     quantity.parentNode.setAttribute("data-error-visible", "true");
-//   }
-// }
-
-// submitBtn.addEventListener("click", ()=>{
-//   if(enteredEmail.value.match(mailRegex)) {
-//     console.log('correct');
-//   }
-//   else if(enteredEmail.value == "") {
-//     enteredEmail.parentNode.setAttribute("data-error-visible", "true");
-//   }
-//   else {
-//     console.log('voila')
-//   }
-
-// });
 
